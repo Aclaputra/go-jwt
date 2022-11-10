64d5d4466b052f2b7fb12dbb5b428937936e7617
Aclaputra <muhamadacla22@gmail.com>
2022-11-10 00:53:12 -0600

    feat: initial commit done

---
+++ controller/auth-controller.go
@@ -0,0 +1,34 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type AuthController interface {
+  Login(ctx *gin.Context)
+  Register(ctx *gin.Context)
+}
+
+type authController struct {
+  // di sini masukkan service yang kalian butuh
+  // this is where you put your service
+}
+
+// NewAuthController creates a new instance of AuthController
+func NewAuthController() AuthController {
+  return &authController{}
+}
+
+func (c *authController) Login(ctx *gin.Context) {
+  ctx.JSON(http.StatusOK, gin.H {
+    "message": "hello login",
+  })
+}
+
+func (c *authController) Register(ctx *gin.Context) {
+  ctx.JSON(http.StatusOK, gin.H {
+    "message": "hello register",
+  })
+}
---
+++ server.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/Aclaputra/golang-jwt/config"
+	"github.com/Aclaputra/golang-jwt/controller"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+var (
+  db *gorm.DB = config.SetupDatabaseConnection()
+  authController controller.AuthController
+)
+
+func main() {
+  defer config.CloseDatabaseConnection(db)
+  r := gin.Default()
+
+  authRoutes := r.Group("api/auth")
+  {
+    authRoutes.POST("/login")
+    authRoutes.POST("/register")
+  }
+
+  r.Run()
+}
