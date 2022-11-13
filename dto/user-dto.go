05e115eef91aff8298324492aa525a1be2a66f13
Aclaputra <muhamadacla22@gmail.com>
2022-11-12 22:35:32 -0600

    feat: finish golang jwt

--- dto/user-dto.go
+++ dto/user-dto.go
@@ -1,16 +1,9 @@
 package dto
 
-// UserUpdateDTO is used by client when PUT update profile
+//UserUpdateDTO is used by client when PUT update profile
 type UserUpdateDTO struct {
-  ID uint64 `json:"id" form:"id" binding:"required"`
-  Name string `json:"name" form:"name" binding:"required"`
-  Email string `json:"email" form:"email" binding:"required" validate:"email"`
-  Password string `json:"password.emitempty" form:"password,omitempty" validate:"min:6"`
-}
-
-// UserCreateDTO is used when register a user
-type UserCreateDTO struct {
-  Name string `json:"name" form:"name" binding:"required"`
-  Email string `json:"email" form:"email" binding:"required" validate:"email"`
-  Password string `json:"password.omitempty" form:"password.omitempty" validate:"min:6"`
+	ID       uint64 `json:"id" form:"id"`
+	Name     string `json:"name" form:"name" binding:"required"`
+	Email    string `json:"email" form:"email" binding:"required,email"`
+	Password string `json:"password,omitempty" form:"password,omitempty"`
 }
